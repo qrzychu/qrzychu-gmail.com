651d86059365c1bdc7e12b933650baad6fd52ee9
Krzysztof Skowronek <krzysztof.skowronek@relativity.com>
2020-04-06 20:51:02 +0200

    multiple filters

---
+++ Filters/FilterBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using ReactiveUI;
+using RxUI_Sample.ViewModels;
+
+namespace RxUI_Sample.Filters
+{
+	public abstract class  IFilter : ReactiveObject
+	{
+		private Func<ChildViewModel, bool> _predicate;
+		protected Subject<Unit> _subject = new Subject<Unit>();
+
+		protected IFilter()
+		{
+			PredicateChanged = _subject;
+		}
+
+		public IObservable<Unit> PredicateChanged { get; }
+
+		public Func<ChildViewModel, bool> Predicate
+		{
+			get => _predicate;
+			protected set => this.RaiseAndSetIfChanged(ref _predicate, value);
+		}
+	}
+
+	public class TextFilter : IFilter
+	{
+		private string _text = "";
+
+
+		public TextFilter()
+		{
+			this.WhenAnyValue(x => x.Text)
+				.Select(_ => new Func<ChildViewModel, bool>(model =>
+				{
+					if (!string.IsNullOrEmpty(Text))
+					{
+						return model?.Text?.ToLowerInvariant().Contains(Text.ToLowerInvariant()) ?? false;
+					}
+
+					return true;
+				}))
+				.Subscribe(predicate =>
+				{
+					Predicate = predicate;
+					_subject.OnNext(Unit.Default);
+				});
+		}
+
+		public string Text
+		{
+			get => _text;
+			set => this.RaiseAndSetIfChanged(ref _text, value ?? "");
+		}
+	}
+
+	public class IsSelectedFilter : IFilter
+	{
+		public IsSelectedFilter()
+		{
+			Predicate = vm => vm?.IsSelected ?? false;
+
+			_subject.OnNext(Unit.Default);
+		}
+	}
+
+}
--- ViewModels/ParentViewModel.cs
+++ ViewModels/ParentViewModel.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.DirectoryServices;
+using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Text;
 using System.Windows.Markup;
 using DynamicData;
 using DynamicData.Binding;
 using ReactiveUI;
+using RxUI_Sample.Filters;
 
 namespace RxUI_Sample.ViewModels
 {
@@ -19,6 +22,9 @@ namespace RxUI_Sample.ViewModels
         private string _filterText;
         private ReadOnlyObservableCollection<ChildViewModel> _filteredChildren;
         private ObservableAsPropertyHelper<int> _filterdCount;
+        private SourceList<IFilter> _filters;
+        private ReadOnlyObservableCollection<IFilter> _allFilters;
+        private IObservable<Unit> _filtersChanged;
 
         public ParentViewModel(IScreen screen)
         {
@@ -26,11 +32,19 @@ namespace RxUI_Sample.ViewModels
 
             _childViewModels = new SourceList<ChildViewModel>();
 
+            _childViewModels.AddRange(new []
+            {
+                new ChildViewModel { Text = "abc"},
+                new ChildViewModel { Text = "123"},
+                new ChildViewModel { Text = "abc123"},
+                new ChildViewModel { IsSelected = true},
+            });
+
             var changeSet = _childViewModels.Connect();
 
             changeSet
                 .AutoRefresh(x => x.IsSelected)
-                .Filter(x => x.IsSelected)
+//                .Filter(x => x.IsSelected)
                 .Transform(x => x.WhenAnyValue(vm => vm.IsSelected).Select(_ => x))
                 .DisposeMany()
                 .MergeMany(x => x)
@@ -43,18 +57,23 @@ namespace RxUI_Sample.ViewModels
                 .Subscribe();
 
 
-            // filter is reaplied every time this observable fires
-            var filterObservable = this.WhenAnyValue(x => x.FilterText)
-                .Throttle(TimeSpan.FromMilliseconds(50), RxApp.MainThreadScheduler)
-                .Select(text => new Func<ChildViewModel, bool>(vm =>
-                    string.IsNullOrEmpty(text) || (vm.Text?.ToLowerInvariant().Contains(text.ToLowerInvariant()) ?? false) || vm.IsSelected)
-                    );
+            _filters = new SourceList<IFilter>();
 
+            _filters.Connect()
+	            .Bind(out _allFilters)
+	            .Subscribe();
+
+	        _filtersChanged = _filters.Connect()
+		        .Transform(x => x.PredicateChanged)
+		        .DisposeMany()
+		        .MergeMany(x => x)
+		        .Merge(_filters.CountChanged.Select(x => Unit.Default))
+		        .Do(unit => { });
 
             changeSet
                 .AutoRefresh(x => x.Text)
                 .AutoRefresh(x => x.IsSelected)
-                .Filter(filterObservable)
+                .FilterOnObservable(ApplyFilters, TimeSpan.FromMilliseconds(50))
                 .Bind(out _filteredChildren)
                 .Subscribe();
 
@@ -62,7 +81,7 @@ namespace RxUI_Sample.ViewModels
                 .ToCollection() // this can be expensive, because it creates a full list of items after each change, but until you have 100+ items you are good
                 .Select(x => x.Count)
                 .ToProperty(this, x => x.FilteredCount);
-                    
+
 
             AddChild = ReactiveCommand.Create(() => _childViewModels.Add(new ChildViewModel()));
 
@@ -71,7 +90,23 @@ namespace RxUI_Sample.ViewModels
                 _childViewModels.Remove(vm);
                 vm.IsSelected = true;
             });
+
+            AddTextFilter = ReactiveCommand.Create(() => _filters.Add(new TextFilter()));
+            AddIsSelectedFilter = ReactiveCommand.Create(() => _filters.Add(new IsSelectedFilter()));
+
+            RemoveFilter = ReactiveCommand.Create<IFilter>(filter => _filters.Remove(filter));
         }
+
+        private IObservable<bool> ApplyFilters(ChildViewModel vm)
+        {
+            return _filtersChanged.Select(_ => _allFilters.All(f => f.Predicate?.Invoke(vm) ?? true));
+        }
+
+        public ReactiveCommand<IFilter, Unit> RemoveFilter { get; set; }
+
+        public ReactiveCommand<Unit, Unit> AddIsSelectedFilter { get; set; }
+
+        public ReactiveCommand<Unit, Unit> AddTextFilter { get; set; }
 
         public int FilteredCount => _filterdCount?.Value ?? 0;
 
@@ -84,6 +119,7 @@ namespace RxUI_Sample.ViewModels
         public ReadOnlyObservableCollection<ChildViewModel> FilteredChildren => _filteredChildren;
 
         public ReadOnlyObservableCollection<ChildViewModel> ChildViewModels => _childViewModelsView;
+        public ReadOnlyObservableCollection<IFilter> AllFilters => _allFilters;
 
         public ReactiveCommand<ChildViewModel, Unit> DeleteChild { get; set; }
 
